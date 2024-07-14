86caaa06d368a8907471688d98980a6b07b77238
codehass <hassan.elouardy06@gmail.com>
2024-07-15 00:27:30 +0100

    Create login endpoint to external api

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,35 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
-app.get('/', (c) => c.text('Hono meets Node.js'))
+app.get('/', (c) => c.json({"key":"value b"}));
+
+app.post('/login', async (c) => {
+
+  const { headers, username, password } = await c.req.json();
+   const forwardedHeaders = new Headers();
+   forwardedHeaders.append('Content-Type', 'application/json');
+  for (const key in headers) {
+    forwardedHeaders.append(key, headers[key]);
+  }
+
+  if (!forwardedHeaders.has('x-tenantid')) {
+      forwardedHeaders.append('x-tenantid', 'SchryverPruebas');
+    }
+
+    // Forward the request to external endpoint
+  const response = await fetch('https://7qk9m2xvu2.us-west-2.awsapprunner.com/v1/auth/login', {
+    method: 'POST',
+    headers: forwardedHeaders,
+    body: JSON.stringify({ username, password })
+  });
+
+  const result = await response.json();
+
+  return c.json(result);
+
+})
+
 
 serve(app, (info) => {
-  console.log(`Listening on http://localhost:${info.port}`);
-  ;
+  `Listening on http://localhost:${info.port}`;
 })
